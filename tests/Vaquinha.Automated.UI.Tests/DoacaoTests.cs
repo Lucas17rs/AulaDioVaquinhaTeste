5ed57f9070c7e8b71099b1520fe1d342f04f7665
Lucas <lucasakajiri@hotmail.com>
2021-02-24 12:12:40 -0300

    aula dio stack de testes

--- tests/Vaquinha.Automated.UI.Tests/DoacaoTests.cs
+++ tests/Vaquinha.Automated.UI.Tests/DoacaoTests.cs
@@ -58,6 +58,10 @@ namespace Vaquinha.AutomatedUITests
 			webElement = _driver.FindElement(By.ClassName("btn-yellow"));
 			webElement.Click();
 
+			/*IWebElement campoNome = new _driver.FindElement(By.Id("DadosPessoais_Nome"));
+			campoNome.SendKeys(doacao.DadosPessoais.Nome);*/
+
+
 			//Assert
 			_driver.Url.Should().Contain("/Doacoes/Create");
 		}
